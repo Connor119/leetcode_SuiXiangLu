d74d8c9d1f23eb3741b818d7b2f5673e48f4f352
Connor119 <62894592+Connor119@users.noreply.github.com>
2022-04-24 16:00:48 +0800

    day1

---
+++ src/boshen/com/Array/BinarySearch.java
@@ -0,0 +1,10 @@
+package boshen.com.Array;
+
+/*
+* 数组的二分查找
+* */
+public class BinarySearch {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/boshen/com/Array/basicIdea.java
+++ src/boshen/com/Array/basicIdea.java
@@ -1,4 +1,12 @@
 package boshen.com.Array;
 
+/*探究java中数组是否是连续的*/
 public class basicIdea {
+    public static void main(String[] args) {
+        int[] testArr = {1,2,3};
+        for (int i : testArr) {
+            System.out.println(testArr.hashCode());
+        }
+    }
+
 }
