f31c6a3b01e8d10194ce8b2c970c8dac837a3411
Connor119 <62894592+Connor119@users.noreply.github.com>
2022-04-28 17:08:29 +0800

    day5

--- src/boshen/com/Array/SlidingWindow.java
+++ src/boshen/com/Array/SlidingWindow.java
@@ -1,6 +1,9 @@
 package boshen.com.Array;
 
+import java.util.HashMap;
+
 public class SlidingWindow {
+//    最小滑动窗口
     public static int leetcode209(int target, int[] nums) {
         int subArrLength=Integer.MAX_VALUE;
         int leftIndex = 0;
@@ -15,4 +18,39 @@ public class SlidingWindow {
         }
         return subArrLength==Integer.MAX_VALUE?0:subArrLength;
     }
+//    最大滑动窗口
+    public static int leetcode904(int[] fruits){
+        // int typeCount = 0;
+        int leftPoint = 0;
+        int returnSum = 0;
+        // int returnSumtemp = 0;
+        HashMap<Integer,Integer> recording = new HashMap<>();
+        for(int rightPoint = 0; rightPoint<fruits.length ;rightPoint++){
+            if(!recording.containsKey(fruits[rightPoint])&&recording.size()<2){
+                recording.put(fruits[rightPoint],0);
+            }
+            if(recording.containsKey(fruits[rightPoint])){
+                int recordNum = recording.get(fruits[rightPoint]);
+                recording.put(fruits[rightPoint],recordNum+1);
+            }
+            if(!recording.containsKey(fruits[rightPoint])&&recording.size()==2){
+                returnSum = Math.max(returnSum,rightPoint-leftPoint);
+                while(recording.get(fruits[leftPoint])!=0){
+                    int recordNumRem = recording.get(fruits[leftPoint]);
+                    recording.replace(fruits[leftPoint],--recordNumRem);
+                    if(recording.get(fruits[leftPoint])==0){
+                        recording.remove(fruits[leftPoint]);
+                        leftPoint++;
+                        rightPoint--;
+                        break;
+                    }
+                    leftPoint++;
+                }
+            }
+            returnSum = Math.max(returnSum,rightPoint-leftPoint+1);
+        }
+
+        return returnSum;
+    }
+
 }
