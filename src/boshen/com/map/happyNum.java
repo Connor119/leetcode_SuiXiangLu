dca7850e1e6fe4370631edb3e7229b0d656f1992
Connor119 <62894592+Connor119@users.noreply.github.com>
2022-05-16 16:08:33 +0800

    5.16

---
+++ src/boshen/com/map/happyNum.java
@@ -0,0 +1,29 @@
+package boshen.com.map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class happyNum {
+
+    public boolean isHappy(int n) {
+        Set<Integer> recordSet = new HashSet<>();
+        int sqreSum = 0;
+        while(n>=1){
+            int bit = n%10;
+            n = n/10;
+            sqreSum += bit*bit;
+            if(n<1){
+                if(recordSet.contains(sqreSum)){
+                    return false;
+                }
+                recordSet.add(sqreSum);
+                n = sqreSum;
+                if(n==1){
+                    break;
+                }
+                sqreSum=0;
+            }
+        }
+        return true;
+    }
+}
